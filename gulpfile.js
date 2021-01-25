7278f1fe30e6e94c58afbaa168fddb91d5f47b4e
Lucas Bordeaux <lucabordo@hotmail.co.uk>
2021-01-25 19:51:23 +0000

    Added watchify to the toolset

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,38 @@
 // - TSC (TypeScript compiler) to the source files
 // - Browserify to bundle everything into an application
 // The result is dumped under "dist".
-
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
+var watchify = require("watchify");
 var tsify = require("tsify");
+var fancy_log = require("fancy-log");
 var paths = {
   pages: ["src/*.html"],
 };
+
+var watchedBrowserify = watchify(
+  browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {},
+  }).plugin(tsify)
+);
 
 gulp.task("copy-html", function () {
   return gulp.src(paths.pages).pipe(gulp.dest("dist"));
 });
 
-gulp.task(
-  "default",
-  gulp.series(gulp.parallel("copy-html"), function () {
-    return browserify({
-      basedir: ".",
-      debug: true,
-      entries: ["src/main.ts"],
-      cache: {},
-      packageCache: {},
-    })
-    .plugin(tsify)
+function bundle() {
+  return watchedBrowserify
     .bundle()
+    .on("error", fancy_log)
     .pipe(source("bundle.js"))
     .pipe(gulp.dest("dist"));
-  })
-);
+}
+
+gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", fancy_log);
