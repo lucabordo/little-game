6caf77320eb35bc8f0e4c3abe94188f0bea770fc
Lucas Bordeaux <lucabordo@hotmail.co.uk>
2021-01-28 21:56:44 +0000

    Some propagation to neighbours.

--- src/main.ts
+++ src/main.ts
@@ -59,10 +59,7 @@ export class Board {
         // Create the array with alternative and properly connected cells:
         this.cells = array2d(cellCount, cellCount, function(x, y) {
             const direction = ((x + y) % 2 == 0) ? 'down' : 'up';
-            return new Cell(
-                cellDimension * x, cellDimension * y, 
-                svgRoot, cellDimension, direction
-            );
+            return new Cell(x, y, svgRoot, cellDimension, direction);
         });
     }
 }
@@ -72,7 +69,7 @@ class Game{
     // Current player:
     turn: 'red'|'green';
 
-    constructor(board: Board){
+    constructor(readonly board: Board){
         this.turn = 'red';
         
         // Wire event handling:
@@ -88,25 +85,76 @@ class Game{
      * Event handler for clicks on a cell.
      */
     onCellClick(cell: Cell){
+        // TODO: The color propagated is NOT this.turn in general
+
         report(defaultMessage);
-        const turn = this.turn;
         const opponent = opposite(this.turn);
 
-        if (opposite(turn) == cell.leftColor || opposite(turn) == cell.rightColor){
+        if (opponent == cell.leftColor || opponent == cell.rightColor){
             report(`Player ${this.turn} isn't allowed to play this cell.`);
             return;
         }
-    
-        if (cell.connected){
-            cell.connected = false;
-            // Do something
+
+        cell.connected = !cell.connected;
+        cell.leftColor = this.turn;
+        cell.rightColor = this.turn;
+        this._changeNeighbors(cell, this.turn, 'left');
+        this._changeNeighbors(cell, this.turn, 'right');
+        this.turn = opponent;
+    }
+
+    _changeCornerColor(x: number, y: number, color: 'red'|'green', corner: 'left'|'right'){
+        console.log(`Corner ${x} ${y} ${corner}`);
+        if (0 <= x && x < this.board.cellCount && 0 <= y && y < this.board.cellCount){
+            var cell = this.board.cells[x][y];
+
+            if (corner === 'left'){
+                if (cell.leftColor !== color){
+                    cell.leftColor = color;
+                    this._changeNeighbors(cell, color, corner);
+                    if (cell.connected){
+                        this._changeCornerColor(x, y, color, 'right');
+                    }
+                }
+            }
+            else {
+                if (cell.rightColor !== color){
+                    cell.rightColor = color;
+                    this._changeNeighbors(cell, color, corner);
+                    if (cell.connected){
+                        this._changeCornerColor(x, y, color, 'left');
+                    }
+                }
+            }
+        }
+    }
+
+    _changeNeighbors(cell: Cell, color: 'red'|'green', corner: 'left'|'right'){
+        console.log(`neigh ${cell.x} ${cell.y} ${corner}`);
+        if (cell.direction === 'down'){
+            if (corner === 'left'){
+                this._changeCornerColor(cell.x - 1, cell.y - 1, color, 'right');
+                this._changeCornerColor(cell.x - 1, cell.y - 0, color, 'right');
+                this._changeCornerColor(cell.x - 0, cell.y - 1, color, 'left');
+            }
+            else{
+                this._changeCornerColor(cell.x + 1, cell.y + 1, color, 'left');
+                this._changeCornerColor(cell.x + 1, cell.y + 0, color, 'left');
+                this._changeCornerColor(cell.x + 0, cell.y + 1, color, 'right');
+            }
         }
         else{
-            cell.connected = true;
-            cell.leftColor = turn;
-            cell.rightColor = turn;
+            if (corner === 'left'){
+                this._changeCornerColor(cell.x - 1, cell.y + 1, color, 'right');
+                this._changeCornerColor(cell.x - 1, cell.y + 0, color, 'right');
+                this._changeCornerColor(cell.x + 0, cell.y + 1, color, 'left');
+            }
+            else{
+                this._changeCornerColor(cell.x + 1, cell.y - 1, color, 'left');
+                this._changeCornerColor(cell.x + 1, cell.y + 0, color, 'left');
+                this._changeCornerColor(cell.x + 0, cell.y - 1, color, 'right');
+            }
         }
-        this.turn = opposite(turn);
     }
 }
 
